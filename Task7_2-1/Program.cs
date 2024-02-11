0517c4917333f11bcf2d50aa0d7f71d9698f8072
Konstantin <konstbaz@gmail.com>
2024-02-12 00:23:57 +0300

    Решил задачу 2.2

---
+++ Task7_2-1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        
+        if(M > N)
+        {
+            Console.Write("N должно быть больше M!");
+        }
+        
+        void rec(int M, int N)
+        {
+            if (M > N)
+            {
+                return;
+            }
+            if (M % 2 == 0)    // Тут, собственно и все отличия от задачи 1 :)
+            {
+                Console.Write($"{M} ");
+            }
+            rec(M + 1, N);
+        }
+        
+        rec(M, N);
+    }
+}
